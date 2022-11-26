363772a2225c0e638951d687159e14311f84da9d
DeniConiglio <deniseconiglio2017@outlook.com>
2022-11-25 23:14:15 -0300

    Agregar archivos de proyecto.

---
+++ OperasWebSite/Validations/CheckValidYear.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace OperasWebSite.Validations
+{
+    public class CheckValidYear : ValidationAttribute
+    {
+        public CheckValidYear()
+        {
+            ErrorMessage = "La Opera registrada mas antigua es en 1958 de Rinuccini";
+        }
+
+        public override bool IsValid(object value)
+        {
+            int year = (int)value;
+            if (year < 1598)
+            {
+                return false;
+            }
+            else
+            { return true; }
+        }
+    }
+}
